0712807b8f32ba025347e4ffbf1b30b6e249dd1c
Thiago Barreto de Oliveira <thiago.barreto.oliveira22@gmail.com>
2024-02-14 22:26:14 -0300

    Aula-3

--- src/app.tsx
+++ src/app.tsx
@@ -1,49 +1,118 @@
+import { ChangeEvent, useState } from "react";
 import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
-const notes = [
-    {
-        date: new Date(2023, 14, 1),
-        content:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, aut qui sunt id voluptatem tenetur ullam accusantium enim pariatur autem vero commodi at rerum. Vero consequuntur accusamus fuga accusantium ipsa.",
-    },
-    {
-        date: new Date(2023, 15, 1),
-        content:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, aut qui sunt id voluptatem tenetur ullam accusantium enim pariatur autem vero commodi at rerum. Vero consequuntur accusamus fuga accusantium ipsa.",
-    },
-    {
-        date: new Date(2023, 16, 1),
-        content:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, aut qui sunt id voluptatem tenetur ullam accusantium enim pariatur autem vero commodi at rerum. Vero consequuntur accusamus fuga accusantium ipsa.",
-    },
-    {
-        date: new Date(2023, 17, 1),
-        content:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, aut qui sunt id voluptatem tenetur ullam accusantium enim pariatur autem vero commodi at rerum. Vero consequuntur accusamus fuga accusantium ipsa.",
-    },
-];
+interface Note {
+    id: string;
+    date: Date;
+    content: string;
+}
+
+// const notes: Note[] = [
+//     {
+//         id: "1",
+//         date: new Date(2023, 14, 1),
+//         content:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, aut qui sunt id voluptatem tenetur ullam accusantium enim pariatur autem vero commodi at rerum. Vero consequuntur accusamus fuga accusantium ipsa.",
+//     },
+//     {
+//         id: "2",
+//         date: new Date(2023, 15, 1),
+//         content:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, aut qui sunt id voluptatem tenetur ullam accusantium enim pariatur autem vero commodi at rerum. Vero consequuntur accusamus fuga accusantium ipsa.",
+//     },
+//     {
+//         id: "3",
+//         date: new Date(2023, 16, 1),
+//         content:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, aut qui sunt id voluptatem tenetur ullam accusantium enim pariatur autem vero commodi at rerum. Vero consequuntur accusamus fuga accusantium ipsa.",
+//     },
+//     {
+//         id: "4",
+//         date: new Date(2023, 17, 1),
+//         content:
+//             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, aut qui sunt id voluptatem tenetur ullam accusantium enim pariatur autem vero commodi at rerum. Vero consequuntur accusamus fuga accusantium ipsa.",
+//     },
+// ];
 
 export function App() {
+    const [search, setSearch] = useState("");
+    const [notes, setNotes] = useState<Note[]>(() => {
+        const notesOnStorage = localStorage.getItem("notes");
+
+        if (notesOnStorage) {
+            return JSON.parse(notesOnStorage);
+        }
+
+        return [];
+    });
+
+    function onNoteCreated(content: string) {
+        const newNote = {
+            id: crypto.randomUUID(),
+            date: new Date(),
+            content,
+        };
+
+        const notesArray = [newNote, ...notes];
+
+        setNotes(notesArray);
+
+        localStorage.setItem("notes", JSON.stringify(notesArray));
+    }
+
+    function onNoteDeleted(id: string) {
+        const notesArray = notes.filter((note) => {
+            return note.id !== id;
+        });
+
+        setNotes(notesArray);
+
+        localStorage.setItem("notes", JSON.stringify(notesArray));
+    }
+
+    function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+        const query = event.target.value;
+
+        setSearch(query);
+    }
+
+    const filteredNotes =
+        search !== ""
+            ? notes.filter((note) =>
+                  note.content
+                      .toLocaleLowerCase()
+                      .includes(search.toLocaleLowerCase())
+              )
+            : notes;
+
     return (
-        <div className="mx-auto max-w-6xl my-12  space-y-6">
+        <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
             <img src={logo} alt="NLW Expert" />
 
             <form className="w-full">
                 <input
                     type="text"
                     placeholder="Busque em suas notas..."
-                    className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+                    className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+                    onChange={handleSearch}
                 />
             </form>
 
             <div className="h-px bg-slate-700" />
 
-            <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-                <NewNoteCard />
-                {notes.map((note) => {
-                    return <NoteCard note={note} />;
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
+                <NewNoteCard onNoteCreated={onNoteCreated} />
+
+                {filteredNotes.map((note) => {
+                    return (
+                        <NoteCard
+                            onNoteDeleted={onNoteDeleted}
+                            key={note.id}
+                            note={note}
+                        />
+                    );
                 })}
             </div>
         </div>
